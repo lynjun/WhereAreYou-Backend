009b008c4afcec4dffc2cb024d244afb59a66145
euics <pjh575157@gmail.com>
2023-09-27 00:51:40 +0900

    Initial Commit

---
+++ src/main/java/com/example/whereareyou/WhereareyouApplication.java
@@ -0,0 +1,13 @@
+package com.example.whereareyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereareyouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhereareyouApplication.class, args);
+    }
+
+}
