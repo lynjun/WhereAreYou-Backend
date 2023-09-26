9d7539a0f6960d63d497a256d90b1e12036c3f0f
euics <pjh575157@gmail.com>
2023-09-27 01:58:36 +0900

    Feat: Add updateSchedule Method by JPQL

--- src/main/java/com/example/whereareyou/repository/ScheduleRepository.java
+++ src/main/java/com/example/whereareyou/repository/ScheduleRepository.java
@@ -1,8 +1,14 @@
 package com.example.whereareyou.repository;
 
+import com.example.whereareyou.domain.Member;
 import com.example.whereareyou.domain.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 /**
  * packageName    : project.whereareyou.repository
@@ -17,5 +23,14 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface ScheduleRepository extends JpaRepository<Schedule, String> {
-
+    @Modifying
+    @Query("UPDATE Schedule s SET s.start = :start, s.end = :end, s.title = :title, s.place = :place, s.memo = :memo, s.closed = :closed, s.creator = :creator WHERE s.id = :scheduleId")
+    int updateSchedule(@Param("start") LocalDateTime start,
+                       @Param("end") LocalDateTime end,
+                       @Param("title") String title,
+                       @Param("place") String place,
+                       @Param("memo") String memo,
+                       @Param("closed") Boolean closed,
+                       @Param("creator") Member creator,
+                       @Param("scheduleId") String scheduleId);
 }
