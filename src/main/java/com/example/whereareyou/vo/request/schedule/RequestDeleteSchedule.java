df6701054e3531b81d5b340bdf5d0a0c52654435
euics <pjh575157@gmail.com>
2023-09-27 02:06:23 +0900

    Feat: Add RequestDeleteSchedule

---
+++ src/main/java/com/example/whereareyou/vo/request/schedule/RequestDeleteSchedule.java
@@ -0,0 +1,19 @@
+package com.example.whereareyou.vo.request.schedule;
+
+import lombok.Data;
+
+/**
+ * packageName    : project.whereareyou.vo.request.schedule
+ * fileName       : RequestDeleteSchedule
+ * author         : pjh57
+ * date           : 2023-09-16
+ * description    : 일정 삭제 Request
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-09-16        pjh57       최초 생성
+ */
+@Data
+public class RequestDeleteSchedule {
+    private String scheduleId;
+}
