215a53cd21e19397e16ab2984b51c9921ee33ca6
lynjun97 <lynjun97@naver.com>
2023-10-01 12:23:27 +0900

    Feat: Add EmailDuplicatedException

---
+++ src/main/java/com/example/whereareyou/exception/customexception/EmailDuplicatedException.java
@@ -0,0 +1,11 @@
+package com.example.whereareyou.exception.customexception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class EmailDuplicatedException extends RuntimeException{
+    public EmailDuplicatedException(String m){
+        super(m);
+    }
+}
