736093f16ed8c0ac412542a5df54c20c747fd0cd
lynjun97 <lynjun97@naver.com>
2023-10-01 12:08:58 +0900

    Feat: Add reissueToken method

--- src/main/java/com/example/whereareyou/service/JwtTokenService.java
+++ src/main/java/com/example/whereareyou/service/JwtTokenService.java
@@ -1,16 +1,25 @@
 package com.example.whereareyou.service;
 
+import com.example.whereareyou.dto.TokenDto;
+import com.example.whereareyou.exception.customexception.ExpiredJwt;
 import com.example.whereareyou.exception.customexception.JwtTokenMismatchException;
+import com.example.whereareyou.exception.customexception.TokenNotFound;
 import io.jsonwebtoken.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
+@RequiredArgsConstructor
 @Component
 public class JwtTokenService {
+
+    private final RedisTemplate<String, String > redisTemplate;
 
     @Value("${jwt.secret}")
     private String jwtSecret;
@@ -39,12 +48,31 @@ public class JwtTokenService {
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + refreshTokenExpiration);
 
-        return Jwts.builder()
+        String refreshToken = Jwts.builder()
                 .setSubject(userId)
                 .setIssuedAt(now)
                 .setExpiration(expiryDate)
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
+
+        redisTemplate.opsForValue().set(
+                userId,
+                refreshToken,
+                refreshTokenExpiration,
+                TimeUnit.MILLISECONDS
+        );
+        return refreshToken;
+
+    }
+
+    public void validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+        } catch(ExpiredJwtException e) {
+            throw new ExpiredJwt("만료된 토큰 입니다");
+        } catch(JwtException e) {
+            throw new TokenNotFound("존재 하지 않은 토큰 입니다.+");
+        }
     }
 
     public String validateUserIdFromRequest(HttpServletRequest request, String userId) throws Exception {
@@ -71,5 +99,33 @@ public class JwtTokenService {
         }
 
         throw new JwtException("Invalid JWT token");
+    }
+
+    private String getUserId(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+
+    public TokenDto reissueToken(TokenDto tokenDto){
+
+        validateToken(tokenDto.getRefreshToken());
+
+        String userId = getUserId(tokenDto.getRefreshToken());
+
+        String redisRefreshToken = redisTemplate.opsForValue().get(userId);
+
+        if(!redisRefreshToken.equals(tokenDto.getRefreshToken())){
+            throw new JwtException("유효 하지 않은 토큰 입니다.");
+        }
+
+        tokenDto = new TokenDto(
+                generateAccessToken(userId),
+                generateRefreshToken(userId)
+        );
+        return tokenDto;
     }
 }
