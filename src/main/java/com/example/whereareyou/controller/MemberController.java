6546bb8fd070a2262cde8751268da9056ee8f81a
lynjun97 <lynjun97@naver.com>
2023-10-01 21:08:29 +0900

    Style: 코드 수정

--- src/main/java/com/example/whereareyou/controller/MemberController.java
+++ src/main/java/com/example/whereareyou/controller/MemberController.java
@@ -6,7 +6,6 @@ import com.example.whereareyou.service.MemberService;
 import com.example.whereareyou.vo.response.member.ResponseCheckEmail;
 import com.example.whereareyou.vo.response.member.ResponseCheckId;
 import com.example.whereareyou.vo.response.member.ResponseFindId;
-import com.example.whereareyou.vo.response.member.ResponseResetPassword;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
