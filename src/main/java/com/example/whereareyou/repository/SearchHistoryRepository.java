2c4cb1c30dcf337232e586e66c892f042e2fca64
euics <pjh575157@gmail.com>
2023-10-11 23:14:54 +0900

    Feat: Add findByMember Method
    Member를 통한 SearchHistory List 반환

--- src/main/java/com/example/whereareyou/repository/SearchHistoryRepository.java
+++ src/main/java/com/example/whereareyou/repository/SearchHistoryRepository.java
@@ -1,15 +1,18 @@
 package com.example.whereareyou.repository;
 
+import com.example.whereareyou.domain.Member;
 import com.example.whereareyou.domain.SearchHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * packageName    : com.example.whereareyou.repository
  * fileName       : SearchHistoryRepository
  * author         : pjh57
  * date           : 2023-10-11
- * description    :
+ * description    : 검색 Repository
  * ===========================================================
  * DATE              AUTHOR             NOTE
  * -----------------------------------------------------------
@@ -17,5 +20,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface SearchHistoryRepository extends JpaRepository<SearchHistory, String> {
-
+    List<SearchHistory> findByMember(Member member);
 }
