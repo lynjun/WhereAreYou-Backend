4e339dea3d318ea18a8b0bacdc26397d7d07d858
euics <pjh575157@gmail.com>
2023-10-11 22:53:50 +0900

    Refactor: Add SearchHistoryRepository

---
+++ src/main/java/com/example/whereareyou/repository/SearchHistoryRepository.java
@@ -0,0 +1,21 @@
+package com.example.whereareyou.repository;
+
+import com.example.whereareyou.domain.SearchHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * packageName    : com.example.whereareyou.repository
+ * fileName       : SearchHistoryRepository
+ * author         : pjh57
+ * date           : 2023-10-11
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-10-11        pjh57       최초 생성
+ */
+@Repository
+public interface SearchHistoryRepository extends JpaRepository<SearchHistory, String> {
+
+}
