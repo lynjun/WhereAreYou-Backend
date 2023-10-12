15b7ad0e26d7da4a7a3424c3dd2bad6305ce8c0e
lynjun97 <lynjun97@naver.com>
2023-10-12 20:27:17 +0900

    Feat: Redis to Mysql

--- src/main/java/com/example/whereareyou/service/JwtTokenService.java
+++ src/main/java/com/example/whereareyou/service/JwtTokenService.java
@@ -1,25 +1,25 @@
 package com.example.whereareyou.service;
 
+import com.example.whereareyou.domain.RefreshToken;
 import com.example.whereareyou.dto.TokenDto;
 import com.example.whereareyou.exception.customexception.ExpiredJwt;
 import com.example.whereareyou.exception.customexception.JwtTokenMismatchException;
 import com.example.whereareyou.exception.customexception.TokenNotFound;
+import com.example.whereareyou.repository.RefreshTokenRepository;
 import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 @Component
 public class JwtTokenService {
 
-    private final RedisTemplate<String, String > redisTemplate;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Value("${jwt.secret}")
     private String jwtSecret;
@@ -31,12 +31,12 @@ public class JwtTokenService {
     private long refreshTokenExpiration;
 
     // accessToken 생성
-    public String generateAccessToken(String userId) {
+    public String generateAccessToken(String memberId) {
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + accessTokenExpiration);
 
         return Jwts.builder()
-                .setSubject(userId)
+                .setSubject(memberId)
                 .setIssuedAt(now)
                 .setExpiration(expiryDate)
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
@@ -44,23 +44,30 @@ public class JwtTokenService {
     }
 
     // refreshToken 생성
-    public String generateRefreshToken(String userId) {
+    public String generateRefreshToken(String memberId) {
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + refreshTokenExpiration);
 
         String refreshToken = Jwts.builder()
-                .setSubject(userId)
+                .setSubject(memberId)
                 .setIssuedAt(now)
                 .setExpiration(expiryDate)
                 .signWith(SignatureAlgorithm.HS512, jwtSecret)
                 .compact();
 
-        redisTemplate.opsForValue().set(
-                userId,
-                refreshToken,
-                refreshTokenExpiration,
-                TimeUnit.MILLISECONDS
-        );
+        RefreshToken tokenId = refreshTokenRepository.findByMemberId(memberId);
+
+        if(tokenId != null){
+            refreshTokenRepository.delete(tokenId);
+        }
+
+        RefreshToken token = RefreshToken.builder()
+                .memberId(memberId)
+                .refreshToken(refreshToken)
+                .build();
+
+        refreshTokenRepository.save(token);
+
         return refreshToken;
 
     }
@@ -69,9 +76,9 @@ public class JwtTokenService {
         try{
             Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
         } catch(ExpiredJwtException e) {
-            throw new ExpiredJwt("만료된 토큰 입니다");
+            throw new ExpiredJwt("만료된 토큰 입니다.");
         } catch(JwtException e) {
-            throw new TokenNotFound("존재 하지 않은 토큰 입니다.+");
+            throw new TokenNotFound("존재 하지 않은 토큰 입니다.");
         }
     }
 
@@ -101,7 +108,7 @@ public class JwtTokenService {
         throw new JwtException("Invalid JWT token");
     }
 
-    private String getUserId(String token){
+    private String getMemberId(String token){
         Claims claims = Jwts.parser()
                 .setSigningKey(jwtSecret)
                 .parseClaimsJws(token)
@@ -114,17 +121,11 @@ public class JwtTokenService {
 
         validateToken(tokenDto.getRefreshToken());
 
-        String userId = getUserId(tokenDto.getRefreshToken());
-
-        String redisRefreshToken = redisTemplate.opsForValue().get(userId);
-
-        if(!redisRefreshToken.equals(tokenDto.getRefreshToken())){
-            throw new JwtException("유효 하지 않은 토큰 입니다.");
-        }
+        String memberId = getMemberId(tokenDto.getRefreshToken());
 
         tokenDto = new TokenDto(
-                generateAccessToken(userId),
-                generateRefreshToken(userId)
+                generateAccessToken(memberId),
+                generateRefreshToken(memberId)
         );
         return tokenDto;
     }
