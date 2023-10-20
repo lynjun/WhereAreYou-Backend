438a8933a8a18a7fb2d4639f873aabdd0bc3a21b
lynjun97 <lynjun97@naver.com>
2023-10-21 00:48:35 +0900

    Feat: Add ModifyMyPageDto

---
+++ src/main/java/com/example/whereareyou/dto/ModifyMyPageDto.java
@@ -0,0 +1,15 @@
+package com.example.whereareyou.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class ModifyMyPageDto {
+
+    private String oldId;
+    private String newId;
+
+}
