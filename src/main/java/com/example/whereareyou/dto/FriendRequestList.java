e1a3e82e908ccd03699f1fb557a29a4e96ecb67a
lynjun97 <lynjun97@naver.com>
2023-10-25 23:24:35 +0900

    Feat: Add FriendRequestList Dto

---
+++ src/main/java/com/example/whereareyou/dto/FriendRequestList.java
@@ -0,0 +1,19 @@
+package com.example.whereareyou.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class FriendRequestList {
+
+    String friendRequestId;
+    String senderUserName;
+    String senderUserId;
+
+}
+
