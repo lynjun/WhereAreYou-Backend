2ad15a53a7705260a4b3cdc20b99cd131b987890
lynjun97 <lynjun97@naver.com>
2023-10-26 00:50:20 +0900

    Feat: Add acceptFriend Method

--- src/main/java/com/example/whereareyou/service/FriendService.java
+++ src/main/java/com/example/whereareyou/service/FriendService.java
@@ -1,9 +1,13 @@
 package com.example.whereareyou.service;
 
+import com.example.whereareyou.domain.Friend;
 import com.example.whereareyou.domain.FriendRequest;
 import com.example.whereareyou.domain.Member;
+import com.example.whereareyou.dto.AcceptFriend;
 import com.example.whereareyou.dto.FriendRequestDto;
 import com.example.whereareyou.dto.FriendRequestList;
+import com.example.whereareyou.exception.customexception.FriendRequestNotFoundException;
+import com.example.whereareyou.exception.customexception.MemberMismatchException;
 import com.example.whereareyou.exception.customexception.UserNotFoundException;
 import com.example.whereareyou.repository.FriendRepository;
 import com.example.whereareyou.repository.FriendRequestRepository;
@@ -73,4 +77,35 @@ public class FriendService {
 
         return responseFriendRequestList;
     }
+
+    public void acceptFriend(AcceptFriend acceptFriend){
+        Optional<Member> byUserId = memberRepository.findByUserId(acceptFriend.getUserId());
+        Member userId = byUserId.orElseThrow(() -> new UserNotFoundException("존재하지 않은 userId 입니다."));
+
+        Optional<Member> byFriendId = memberRepository.findByUserId(acceptFriend.getFriendId());
+        Member friendId = byFriendId.orElseThrow(() -> new UserNotFoundException("존재하지 않은 FriendId 입니다."));
+
+        Optional<FriendRequest> byId = friendRequestRepository.findById(acceptFriend.getFriendRequestId());
+        FriendRequest friendRequest = byId.orElseThrow(() -> new FriendRequestNotFoundException("존재하지 않은 요청 입니다."));
+
+        if(friendRequest.getMember()!=userId){
+            throw new MemberMismatchException("회원 정보가 일치하지 않습니다");
+        }
+
+        friendRequestRepository.delete(friendRequest);
+
+        Friend user = Friend.builder()
+                .member(userId)
+                .friend(acceptFriend.getUserId())
+                .build();
+        friendRepository.save(user);
+
+        Friend friend = Friend.builder()
+                .member(friendId)
+                .friend(acceptFriend.getFriendId())
+                .build();
+        friendRepository.save(friend);
+
+    }
+
 }
