c427d2bc1533977bbc6aac0f391dc87c07760cec
lynjun97 <lynjun97@naver.com>
2023-10-29 16:49:12 +0900

    Feat: Add FriendList Dto

---
+++ src/main/java/com/example/whereareyou/dto/FriendList.java
@@ -0,0 +1,16 @@
+package com.example.whereareyou.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class FriendList {
+
+    private String friendId;
+
+}
