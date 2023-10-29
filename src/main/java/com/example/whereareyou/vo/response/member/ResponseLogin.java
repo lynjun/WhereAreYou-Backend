25fe13558b789a60a89b50d5945e77890ab870ba
lynjun97 <lynjun97@naver.com>
2023-10-29 16:52:01 +0900

    Refactor: Add friendList Variable

--- src/main/java/com/example/whereareyou/vo/response/member/ResponseLogin.java
+++ src/main/java/com/example/whereareyou/vo/response/member/ResponseLogin.java
@@ -1,16 +1,18 @@
 package com.example.whereareyou.vo.response.member;
 
+import com.example.whereareyou.dto.FriendList;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseLogin {
 
-    private String message;
     private String accessToken;
     private String refreshToken;
     private String memberId;
-
+    private List<FriendList> friendList;
 
 }
