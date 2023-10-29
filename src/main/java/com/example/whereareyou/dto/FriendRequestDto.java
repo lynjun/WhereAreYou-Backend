9de8c67a84ce2e40a6457e7054a9b7a618ddfa6a
lynjun97 <lynjun97@naver.com>
2023-10-29 17:07:47 +0900

    Refactor : Modify memberIdList to friendId

--- src/main/java/com/example/whereareyou/dto/FriendRequestDto.java
+++ src/main/java/com/example/whereareyou/dto/FriendRequestDto.java
@@ -4,13 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class FriendRequestDto {
 
-    private List<String> memberIdList; // 친추 보낼 아이디들
-    private String userId; // 친추 보낸 아이디
+    private String friendId; // 친추 보낼 아이디
+    private String memberId; // 친추 보낸 아이디
 }
