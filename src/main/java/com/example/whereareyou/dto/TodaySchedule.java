6a5734aff2c765a8bb59990712df5336890a32a7
lynjun97 <lynjun97@naver.com>
2023-11-02 17:57:49 +0900

    Feat : Add TodaySchedule Dto

---
+++ src/main/java/com/example/whereareyou/dto/TodaySchedule.java
@@ -0,0 +1,18 @@
+package com.example.whereareyou.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class TodaySchedule {
+
+    private List<ScheduleList> scheduleList;
+
+}
