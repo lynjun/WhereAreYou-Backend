ca744d505973be0f9d16e6b60eae0bb29777d506
lynjun97 <lynjun97@naver.com>
2023-11-02 17:12:53 +0900

    Feat : Add RefuseFriend Dto

---
+++ src/main/java/com/example/whereareyou/dto/RefuseFriend.java
@@ -0,0 +1,14 @@
+package com.example.whereareyou.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+public class RefuseFriend {
+
+    private String friendRequestId;
+
+}
