4df04e65fb38816e0bb09424ad786458ff6fe6c5
euics <pjh575157@gmail.com>
2023-11-20 01:50:37 +0900

    feat: Add RequestCreateGroup
    - 그룹 생성 Request 클래스
    - ownerId: 그룹 생성자
    - groupMemberId: 그룹에 들어가는 Member

---
+++ src/main/java/com/example/whereareyou/friendGroup/request/RequestCreateGroup.java
@@ -0,0 +1,22 @@
+package com.example.whereareyou.friendGroup.request;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * packageName    : com.example.whereareyou.friendGroup.request
+ * fileName       : RequestCreateGroup
+ * author         : pjh57
+ * date           : 2023-11-20
+ * description    : 그룹 생성 Request
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-11-20        pjh57       최초 생성
+ */
+@Data
+public class RequestCreateGroup {
+    private String ownerId;
+    private List<String> groupMemberId;
+}
