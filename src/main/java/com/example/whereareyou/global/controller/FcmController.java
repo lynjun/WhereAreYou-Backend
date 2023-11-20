d5ec6c0206245bef981869ff7b446631fbb47608
euics <pjh575157@gmail.com>
2023-11-20 23:22:50 +0900

    feat: saveFcmToken 메서드 구현
    - 새로 로그인한 회원이라면 토큰 저장
    - 만약 이미 토큰이 존재한다면 갱신

--- src/main/java/com/example/whereareyou/global/controller/FcmController.java
+++ src/main/java/com/example/whereareyou/global/controller/FcmController.java
@@ -1,21 +1,31 @@
 package com.example.whereareyou.global.controller;
 
 import com.example.whereareyou.global.dto.FirebaseCloudMessageDTO;
+import com.example.whereareyou.global.request.RequestFcmToken;
+import com.example.whereareyou.global.service.FcmTokenService;
 import com.example.whereareyou.global.service.FirebaseCloudMessageService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @RestController
-@RequiredArgsConstructor
+@RequestMapping("/fcm")
 public class FcmController {
     private final FirebaseCloudMessageService firebaseCloudMessageService;
+    private final FcmTokenService fcmTokenService;
 
-    @PostMapping("/api/fcm")
+    @Autowired
+    public FcmController(FirebaseCloudMessageService firebaseCloudMessageService, FcmTokenService fcmTokenService) {
+        this.firebaseCloudMessageService = firebaseCloudMessageService;
+        this.fcmTokenService = fcmTokenService;
+    }
+
+    @PostMapping("/message")
     public ResponseEntity pushMessage(@RequestBody FirebaseCloudMessageDTO requestDTO) throws IOException {
         System.out.println(requestDTO.getTargetToken() + " "
                 +requestDTO.getTitle() + " " + requestDTO.getBody());
@@ -25,5 +35,12 @@ public class FcmController {
                 requestDTO.getTitle(),
                 requestDTO.getBody());
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> saveFcmToken(@RequestBody RequestFcmToken requestFcmToken){
+        fcmTokenService.saveOrUpdateToken(requestFcmToken);
+
+        return ResponseEntity.noContent().build();
     }
 }
