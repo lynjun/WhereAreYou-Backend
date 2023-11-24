5eca04f831566e49e3816a474c37b553f27bfcd1
euics <pjh575157@gmail.com>
2023-11-25 00:13:09 +0900

    style: 클래스 단위 Javadoc 주석 제거

--- src/main/java/com/example/whereareyou/searchHistory/repository/SearchHistoryRepository.java
+++ src/main/java/com/example/whereareyou/searchHistory/repository/SearchHistoryRepository.java
@@ -7,17 +7,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-/**
- * packageName    : com.example.whereareyou.repository
- * fileName       : SearchHistoryRepository
- * author         : pjh57
- * date           : 2023-10-11
- * description    : 검색 Repository
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2023-10-11        pjh57       최초 생성
- */
 @Repository
 public interface SearchHistoryRepository extends JpaRepository<SearchHistory, String> {
     List<SearchHistory> findByMember(Member member);
--- src/main/java/com/example/whereareyou/searchHistory/response/ResponseSearchHistory.java
+++ src/main/java/com/example/whereareyou/searchHistory/response/ResponseSearchHistory.java
@@ -5,17 +5,6 @@ import lombok.Data;
 
 import java.util.ArrayList;
 
-/**
- * packageName    : com.example.whereareyou.vo.response.searchHistory
- * fileName       : ResponseSearchHistory
- * author         : pjh57
- * date           : 2023-10-11
- * description    : 사용자 검색 기록 조회
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2023-10-11        pjh57       최초 생성
- */
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseSearchHistory {
