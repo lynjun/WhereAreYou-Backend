05a53ece74ee7e20e9e1342d439725b27465a677
euics <pjh575157@gmail.com>
2023-11-24 19:06:13 +0900

    feat: ExceptionConstant 클래스 필드 추가
    - Schedule과 관련된 Exception Message 필드 추가

--- src/main/java/com/example/whereareyou/global/constant/ExceptionConstant.java
+++ src/main/java/com/example/whereareyou/global/constant/ExceptionConstant.java
@@ -3,4 +3,8 @@ package com.example.whereareyou.global.constant;
 public class ExceptionConstant {
     public static final String MEMBER_ID_CANNOT_BE_IN_FRIEND_LIST = "일정 친구 추가 시 본인의 ID는 들어갈 수 없습니다.";
     public static final String USER_NOT_FOUND_EXCEPTION_MESSAGE = "존재하지 않는 memberId입니다.";
+    public static final String SCHEDULE_NOT_FOUND_EXCEPTION_MESSAGE = "존재하지 않는 scheduleId입니다.";
+    public static final String SCHEDULE_CREATOR_MISMATCH_EXCEPTION_MESSAGE = "해당 schedule의 creator가 아닙니다.";
+    public static final String UPDATE_QUERY_EXCEPTION_MESSAGE = "업데이트 실패";
+    public static final int UPDATE_QUERY_EXCEPTION_SIZE = 0;
 }
