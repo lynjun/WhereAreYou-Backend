2af8db39e6e0d5a9c58557c8356eeaff13c33c98
euics <pjh575157@gmail.com>
2023-11-24 18:48:49 +0900

    feat: Add ScheduleConstant
    - ScheduleService의 Magic Number 관리 클래스

---
+++ src/main/java/com/example/whereareyou/schedule/constant/ScheduleConstant.java
@@ -0,0 +1,7 @@
+package com.example.whereareyou.schedule.constant;
+
+public class ScheduleConstant {
+    public static final String FCM_MESSAGE_TITLE = "일정 요청이 들어왔습니다.";
+    public static final String FCM_MESSAGE_FROM = "이(가) ";
+    public static final String FCM_MESSAGE_TO = "이(가) ";
+}
