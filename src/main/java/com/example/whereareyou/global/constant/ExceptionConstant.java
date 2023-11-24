b4615e6a298dfe923a16b4299a6f422b5be34e85
euics <pjh575157@gmail.com>
2023-11-24 18:49:05 +0900

    feat: Add ExceptionConstant
    - 예외처리 메세지 관리 Constant

---
+++ src/main/java/com/example/whereareyou/global/constant/ExceptionConstant.java
@@ -0,0 +1,6 @@
+package com.example.whereareyou.global.constant;
+
+public class ExceptionConstant {
+    public static final String MEMBER_ID_CANNOT_BE_IN_FRIEND_LIST = "일정 친구 추가 시 본인의 ID는 들어갈 수 없습니다.";
+    public static final String USER_NOT_FOUND_EXCEPTION_MESSAGE = "존재하지 않는 memberId입니다.";
+}
