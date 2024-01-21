e6dd35d1ea2d0002253502ba6e5477e8e6a8c0db
lynjun <lynjun97@naver.com>
2024-01-21 21:27:47 +0900

    Remove : 불필요한 변수 제거

--- src/main/java/com/example/whereareyou/friend/constant/FriendConstant.java
+++ src/main/java/com/example/whereareyou/friend/constant/FriendConstant.java
@@ -3,11 +3,7 @@ package com.example.whereareyou.friend.constant;
 public class FriendConstant {
     public static final String FRIEND_REQUEST_MESSAGE_TITLE = "새로운 친구 요청";
     public static final String FRIEND_ACCEPT_MESSAGE_TITLE = "친구 요청 수락";
-    public static final String FCM_MESSAGE_FROM = "이(가) ";
     public static final String FCM_MESSAGE_TO = "님이 ";
     public static final String FRIEND_BODY = "친구 요청을 보냈습니다.";
     public static final String ACCEPT_BODY = "친구 요청을 수락했습니다.";
-    public static final int START_OF_MONTH = 1;
-    public static final int END_OF_MONTH = 12;
-    public static final int ZERO = 0;
 }
